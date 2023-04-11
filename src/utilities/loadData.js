de40492b8d816ba20635fcbb7a8dc2b9fd792984
WakilMahmud <wakilmahmud30@gmail.com>
2023-04-11 07:46:50 +0600

    Navigate route problem is fixed

--- src/utilities/loadData.js
+++ src/utilities/loadData.js
@@ -1,8 +1,8 @@
 const loadData = async () => {
-	const loadedJobCategories = await fetch("JobCategories.json");
+	const loadedJobCategories = await fetch("/JobCategories.json");
 	const jobCategories = await loadedJobCategories.json();
 
-	const loadedFeaturedJobs = await fetch("FeaturedJobs.json");
+	const loadedFeaturedJobs = await fetch("/FeaturedJobs.json");
 	const featuredJobs = await loadedFeaturedJobs.json();
 
 	return { jobCategories, featuredJobs };
